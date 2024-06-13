45819a3546659638d9b6eb0c1e46d9cecbf1b3f3
AditiSingh1208 <123548974+AditiSingh1208@users.noreply.github.com>
2024-06-13 12:04:24 +0000

    Added Models and Controllers

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const express = require("express");
+const dotenv = require("dotenv");
+
+const DbConnection = require("./databaseConnection.js");
 
 const userRouter = require("./routes/users.js");
-const booksRouter = require("./routes/books");
+const booksRouter = require("./routes/books.js");
+
+dotenv.config();
 
 const app = express();
+
+DbConnection(); // Invoke the database connection function if needed
 
 const PORT = 8081;
 
 app.use(express.json());
 
-http: app.get("/", (req, res) => {
+// http://localhost:8081/users/
+app.get("/", (req, res) => {
   res.status(200).json({
     message: "Server is up and running :-)",
     data: "hey",
@@ -18,16 +26,12 @@ http: app.get("/", (req, res) => {
 
 app.use("/users", userRouter);
 app.use("/books", booksRouter);
- 
 
-
-// app.get("*", (req, res) => {
-//   res.status(404).json({
-//     message: "This route doesn't exits",
-// });
-
-
-
+app.get("*", (req, res) => {
+  res.status(404).json({
+    message: "This route doesn't exist",
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running at port ${PORT}`);
